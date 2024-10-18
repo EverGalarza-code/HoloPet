22bbc0e07e6407e6429a7d99ae0427a6d826b4c4
EverGalarza-code <evergalarza52@gmail.com>
2024-10-18 07:39:27 -0500

    n
    n

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HoloPets</title>
+    <link rel="stylesheet" href="styles.css">
+    
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <h1 class="topHeader">HoloPets</h1>
+        <p class="paragraph"></p>
+
+
+</body>
+</html>
---
+++ jsscripts/script.js
@@ -0,0 +1,37 @@
+/*import "./styles.css";
+import { computePosition, shift, flip, offset } from "@floating-ui/dom"; */
+
+//select floating id from html
+/*
+const floating = document.getElementById("floating");
+*/
+//listens for mouse movement
+/*
+document.addEventListener("mousemove", ({ clientX, clientY }) => {
+  const virtualEl = {
+  // gets the mouse dimensions and position
+    getBoundingClientRect() {
+      return {
+        width: 0,
+        height: 0,
+        x: clientX,
+        y: clientY,
+        left: clientX,
+        right: clientX,
+        top: clientY,
+        bottom: clientY
+      };
+    }
+  };
+//calculates the position and updates the position
+  computePosition(virtualEl, floating, {
+    placement: "right-start",
+    middleware: [offset(5), flip(), shift()]
+  }).then(({ x, y }) => {
+    Object.assign(floating.style, {
+      top: `${y}px`,
+      left: `${x}px`
+    });
+  });
+});
+*/
